632b44d9b247ee3d6bbaa4064335a8571c69eaa5
Timothy Lee <timothylee3508@gmail.com>
2019-11-14 14:36:38 -0800

    working on displayStats

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,53 +2,83 @@ $(document).ready(initializeApp);
 
 var firstCardClicked = null;
 var secondCardClicked = null;
-var matches = null;
+var matches = 0;
 var max_matches = 2;
+var attempts = 0;
+var games_played = 0;
 
 function initializeApp () {
   $('.card').on('click',handleCardClick);
   $('.modalContainer').addClass('hidden');
-  // blah();
-  //$('.restartClick).on('click',resetGame);
+  displayStats();
+  //$('.restartClick').on('click',resetStats);
 }
 
+
 function handleCardClick(event) {
-  $(event.currentTarget).find('.back').addClass('hidden');
 
-
-  if (firstCardClicked === null) {
-    firstCardClicked = $(event.currentTarget);
-  } else {
-    secondCardClicked = $(event.currentTarget);
+  if ($(event.currentTarget).find('.back').hasClass('hidden')) {
+    return;
   }
-  if (firstCardClicked !== null && secondCardClicked !== null) {
+      $(event.currentTarget).find('.back').addClass('hidden');
+      if (firstCardClicked === null) {
+        firstCardClicked = $(event.currentTarget);
+      } else {
+        secondCardClicked = $(event.currentTarget);
         var firstChoice = $(firstCardClicked).find('.front').css('background-image');
         var secondChoice = $(secondCardClicked).find('.front').css('background-image');
 
-      if(firstChoice === secondChoice) {
-        matches += 1;
-        console.log('The cards do match! :)');
-        firstCardClicked = null;
-        secondCardClicked = null;
-      }else {
-          setTimeout(function () {
-          firstCardClicked.find('.back').removeClass('hidden');
-          secondCardClicked.find('.back').removeClass('hidden');
-          firstCardClicked = null; //set the cards back to null because if you dont, when you choose a third card, the firstCardClicked will still be the 'card' you clicked previously.. its still being assigned to that one. so you gotta set it back to 'null'.
+        if (firstChoice === secondChoice) {
+          matches++;
+          attempts++;
+          console.log('The cards do match! :)');
+          firstCardClicked = null;
           secondCardClicked = null;
+        } else {
+          attempts++;
+          setTimeout(function () {
+            firstCardClicked.find('.back').removeClass('hidden');
+            secondCardClicked.find('.back').removeClass('hidden');
+            firstCardClicked = null; //set the cards back to null because if you dont, when you choose a third card, the firstCardClicked will still be the 'card' you clicked previously.. its still being assigned to that one. so you gotta set it back to 'null'.
+            secondCardClicked = null;
           }, 1500);
           console.log('The cards do not match :(')
-    }
-  }
-  winMatch();
+        }
+      }
+      displayStats();
+    if (matches === max_matches) {
+      $('.modalContainer').removeClass('hidden');
+      games_played++;
+   }
+
 }
 
-function winMatch() {
-   if(matches === max_matches) {
-      $('.modalContainer').removeClass('hidden');
-   }
-};
+function calculateAccuracy () {
+  console.log(matches/attempts);
+  return (matches / attempts);
+}
+function displayStats () {
+  console.log('****TEST****')
+  var accuracy = calculateAccuracy();
+  console.log("attempts: ", attempts);
+  console.log("games played: ", games_played);
+  $('.gamesPlayedNum').text(games_played);
+  $('.attemptsNum').text(attempts);
+  $('.accuracyNum').text(accuracy + '%');
+}
 
-// function resetGame() {
 
+
+
+// function resetStats() {
+//   matches = null;
+//   attempts = null;
+//   games_played++;
+//   displayStats();
+//   $('.modalContainer').addClass('hidden');
+//   $('card').find('.back').removeClass('hidden');
 // }
+
+
+/* ----------- Notes ------------- */
+/* 1.fix the bug ***  when you click on the same image four times --> wins! */
