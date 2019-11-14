018e49b10b424e1220e98349a40b76606cf7f2f8
Timothy Lee <timothylee3508@gmail.com>
2019-11-14 00:28:11 -0800

    finished match two cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,40 @@
 $(document).ready(initializeApp);
 
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+
 function initializeApp () {
- $('.card').on('click',handleCardClick);
+  $('.card').on('click',handleCardClick);
 }
 
-function handleCardClick (event) {
-      $(event.currentTarget).find('.back').addClass('hidden');
+function handleCardClick(event) {
+  $(event.currentTarget).find('.back').addClass('hidden');
+
+
+  if (firstCardClicked === null) {
+    firstCardClicked = $(event.currentTarget);
+    // firstCardClicked.find('.back').addClass('hidden');
+  } else {
+    secondCardClicked = $(event.currentTarget);
+    // secondCardClicked.find('.back').addClass('hidden');
+  }
+  if (firstCardClicked !== null && secondCardClicked !== null) {
+        var firstChoice = $(firstCardClicked).find('.front').css('background-image');
+        var secondChoice = $(secondCardClicked).find('.front').css('background-image');
+
+      if(firstChoice === secondChoice) {
+        matches += 1;
+        console.log('cards match!');
+        firstCardClicked = null;
+        secondCardClicked = null;
+      }else {
+          setTimeout(function () {
+          firstCardClicked.find('.back').removeClass('hidden');
+          secondCardClicked.find('.back').removeClass('hidden');
+          firstCardClicked = null;
+          secondCardClicked = null;
+          }, 1500);
+    }
+  }
 }
