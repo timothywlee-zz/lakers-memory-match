d75a744740bca732a5d83da8f0b09890025ce38a
Timothy Lee <timothylee3508@gmail.com>
2019-11-16 10:59:22 -0800

    working on writing what each line of code is doing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,45 @@
-$(document).ready(initializeApp);
+$(document).ready(initializeApp); //DOM manipulation where the function 'initializeapp' is passed into the document object where it is passed into the ready() method that executes all jquery and javascript code.
 
-var firstCardClicked = null;
-var secondCardClicked = null;
-var matches = 0;
-var max_matches = 2;
-var attempts = 0;
-var games_played = 0;
+var firstCardClicked = null; //when there is no current value for the first click
+var secondCardClicked = null; //when there is no current value for the second click
+var matches = 0; //the total number of card pairs that were matched
+var max_matches = 2; //the max number of card pairs that can be matched for the game to end
+var attempts = 0; //the current number of attempts to match a pair in a single game (attempts+=1 after two clicks)
+var games_played = 0; //the total number of games played (games_played+=1 after win per game)
 
 
-function initializeApp () {
-    $('.modalContainer').addClass("hidden");
-    randomizeCards();
-    $('.card').on('click',handleCardClick);
-    // $('.container').on('click','.card', handleCardClick);
-    $('.restartClick').on('click',resetStats);
+function initializeApp () { //the function handler that executes all the other functions immediately after the document is loaded.
+    $('.modalContainer').addClass("hidden"); //hides the modal Container immediately when document is executed so the user won't see it at first until it is called back
+    randomizeCards(); //executes the randomizeCards function
+    $('.card').on('click',handleCardClick); //when a card is clicked, it would run the function 'handleCardClick'
+    $('.restartClick').on('click',resetStats); //allows the user to click 'play again' which runs the function 'resetStats' that restarts the game
 }
 
-function randomizeCards() {
+function randomizeCards() { //the function that executes code to shuffle the cards randomly
   var cardDeck = ['css-logo', 'css-logo', 'docker-logo', 'docker-logo', 'gitHub-logo', 'gitHub-logo',
                   'html-logo', 'html-logo', 'js-logo', 'js-logo', 'mysql-logo', 'mysql-logo',
-                  'node-logo', 'node-logo', 'php-logo', 'php-logo', 'react-logo', 'react-logo'];
+                  'node-logo', 'node-logo', 'php-logo', 'php-logo', 'react-logo', 'react-logo']; //an array that holds the images
 
-  while (cardDeck.length > 0) {
-    var createCard = $('<div>').addClass('card');
-    var createFront = $('<div>').addClass('front');
-    var createBack = $('<div>').addClass('back');
+  while (cardDeck.length > 0) { // execute this while-loop only when the cardDeck has a current value that is greater than 0.
+    var createCard = $('<div>').addClass('card'); // makes a div per card made
+    var createFront = $('<div>').addClass('front'); //makes a div for the front of each card
+    var createBack = $('<div>').addClass('back'); //makes a div for the back of each card
 
-    var randomIndex = Math.floor(Math.random() * cardDeck.length);
+    var randomIndex = Math.floor(Math.random() * cardDeck.length); //random method used to randomize the images in the cardDeck array.
 
-    var removeACardFromCardDeck = cardDeck.splice(randomIndex, 1);
+    var removeACardFromCardDeck = cardDeck.splice(randomIndex, 1); //removes one random card from the cardDeck array
 
-    var addCardToFront = createFront.addClass(removeACardFromCardDeck); //what does this mean
+    var addCardToFront = createFront.addClass(removeACardFromCardDeck); //where the single random card that was removed from the array is now added to the div with the class of front.
 
     // createFront.appendTo(createCard);
     // createBack.appendTo(createCard);
     // createCard.appendTo('.container');
 
-    $('.container').append((createCard).append(createFront,createBack));
+    $('.container').append((createCard).append(createFront,createBack)); // where the divs with the front and back images --***are added***--> to the div that has the class 'card' --***is added***--> to the div with a class of 'container'
   }
 }
 
-function removeClickHandler (){
+function removeClickHandler (){ //
     $(".card").off("click", handleCardClick)
 }
 
